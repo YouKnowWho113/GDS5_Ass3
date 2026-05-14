0d148489843a092a86e331e158b57e0bf13f9007
YellowZDSh <yellow762001@gmail.com>
2026-05-14 21:05:05 +0700

    Updated Button Highlights

--- Assets/Scripts/NoteBookToggle.cs
+++ Assets/Scripts/NoteBookToggle.cs
@@ -7,6 +7,7 @@ public class NoteBookToggle : MonoBehaviour
 {
     private static GameObject journalPersist;
     public GameObject journal;
+    public GameObject journalButton;
     public NoteBookEvidenceUI evidenceUI;
     int curLevel;
 
@@ -20,6 +21,8 @@ public class NoteBookToggle : MonoBehaviour
 
         else if (journalPersist != gameObject)
             Destroy(gameObject);
+
+        journal.SetActive(false);
     }
 
     private void Update()
@@ -44,66 +47,78 @@ public class NoteBookToggle : MonoBehaviour
 
     public void MainMenu()
     {
+        journal.SetActive(false);
+        journalButton.SetActive(false);
         SceneManager.LoadScene(0);
     }
 
     public void LoadLv1()
     {
         curLevel = 1;
+        journal.SetActive(false);
         SceneManager.LoadScene(1);
     }
 
     public void LoadLv2()
     {
         curLevel = 2;
+        journal.SetActive(false);
         SceneManager.LoadScene(2);
     }
 
     public void LoadLv3()
     {
         curLevel = 3;
+        journal.SetActive(false);
         SceneManager.LoadScene(3);
     }
 
     public void LoadLv4()
     {
         curLevel = 4;
+        journal.SetActive(false);
         SceneManager.LoadScene(4);
     }
 
     public void LoadLv5()
     {
         curLevel = 5;
+        journal.SetActive(false);
         SceneManager.LoadScene(5);
     }
 
     public void LoadLv6()
     {
         curLevel = 6;
+        journal.SetActive(false);
         SceneManager.LoadScene(6);
     }
 
     public void LoadLv7()
     {
         curLevel = 7;
+        journal.SetActive(false);
         SceneManager.LoadScene(7);
     }
 
     public void LoadLv8()
     {
         curLevel = 8;
+        journal.SetActive(false);
         SceneManager.LoadScene(8);
     }
 
     public void LoadLv9()
     {
         curLevel = 9;
+        journal.SetActive(false);
         SceneManager.LoadScene(9);
     }
 
     public void LoadLv10()
     {
         curLevel = 10;
+        journal.SetActive(false);
         SceneManager.LoadScene(10);
     }
 }
